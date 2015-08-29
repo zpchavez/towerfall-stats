21789a3004fe7e026994018080cf6a26dff5d733
Zachary Chavez <zach@rychavez.com>
2015-08-28 20:30:59 -0700

    Make colors and parser modules available.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = {
+    colors      : require('./lib/colors'),
     config      : require('./lib/config'),
     configurer  : require('./lib/configurer'),
-    fileHandler : require('./lib/file-handler')
+    fileHandler : require('./lib/file-handler'),
+    parser      : require('./lib/parser')
 };

58e6c21d3ac269ec188b4cad28e0c01c8791c568
Zachary Chavez <zach@rychavez.com>
2015-09-12 16:40:27 -0700

    Export configDefaults in index.js.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 module.exports = {
-    colors      : require('./lib/colors'),
-    config      : require('./lib/config'),
-    configurer  : require('./lib/configurer'),
-    fileHandler : require('./lib/file-handler'),
-    parser      : require('./lib/parser')
+    colors         : require('./lib/colors'),
+    config         : require('./lib/config'),
+    configDefaults : require('./lib/config-defaults.js'),
+    configurer     : require('./lib/configurer'),
+    fileHandler    : require('./lib/file-handler'),
+    parser         : require('./lib/parser')
 };
